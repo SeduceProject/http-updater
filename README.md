8a0ed5e88bf364b60281383e894d2f920c845f4b
RP <remy.pottier@imt-atlantique.fr>
2020-11-24 09:14:33 +0100

    Updater v1.0

---
+++ README.md
@@ -0,0 +1,6 @@
+### HTTP updater
+* Update the links of the HTTP server by browsing the `/var/www/html` directory
+* Run this script as root after uploading your files to `/var/www/html`
+```
+bash update-download-links.sh
+```
---
+++ update-download-links.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+INDEX="index.lighttpd.html"
+main_title=""
+last_path=""
+
+
+sed -e '/<body>/,$d' $INDEX > /var/www/html/$INDEX
+cd /var/www/html
+chown -R www-data.www-data linux tinycore vmware windows
+
+echo '    <body>' >> $INDEX
+for file in $(find . -type f | grep -v seduce_doc | sed 's/^.//' | sort); do
+	path=$(echo ${file%/*})
+	name=$(echo ${file##*/})
+	if [ ! -z "$path" ]; then
+		if [[ $path != $last_path ]];then
+            if [ -n "$last_path" ]; then
+			    echo "          </div><!-- sub-title -->" >> $INDEX
+            fi
+			# remove all chars except the slashes
+			nb_slash=$(echo -n ${path//[^\/]} | wc -c)
+			for i in $(seq 2 $(( $nb_slash + 1))); do
+				title=$(echo $path | cut -d'/' -f$i)
+				if [ $i -eq 2 ]; then
+					if [[ $main_title != $title ]]; then
+						if [ -n "$main_title" ]; then
+							echo "      </div><!-- main-title -->" >> $INDEX
+						fi
+						main_title=$title
+						echo "      <div class='main-title'>" >> $INDEX
+						echo "        <div>$title</div>" >> $INDEX
+					fi
+				else
+					echo "          <div class='sub-title'>" >> $INDEX
+					echo "            <div>$title</div>" >> $INDEX
+				fi
+			done
+			last_path=$path
+		fi
+		echo  "+ $name"
+		relative_path=$(echo $file | sed 's/^.//')
+		echo "              <a href='$relative_path'>$name</a><br/><br/>" >> $INDEX
+	fi
+done
+echo "          </div><!-- sub-title -->" >> $INDEX
+echo "      </div><!-- main-title -->" >> $INDEX
+echo '    </body>' >> $INDEX
+echo '</html>' >> $INDEX
